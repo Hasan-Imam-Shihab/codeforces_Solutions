a2b947ca9d53b5a980e1163a6a2bf75617f7677f
Md. Hasan Imam Shihab <hasan.shihabimam@gmail.com>
2022-06-19 00:59:18 +0600

    add comment

--- watermelon.cpp
+++ watermelon.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
+    //declare variable 
     int w;
+    //take int input
     cin >> w;
+    //compare if the number is between 1 to 100
     if(w>=0 &&w<=100){
+        //check if it is devided into even except 2
         if(w%2==0 && w!=2){
             cout << "YES" << endl;
         }
